3bc32abd1ca4789cb707a66f6cccc6ba4ef92ddb
piojosnos <piojosnos@gmail.com>
2011-02-27 01:37:58 +0000

    Almost final base implemtation for pushbox (4)

--- UnsortedSandBox/src/boxpush4/MultiMain.java
+++ UnsortedSandBox/src/boxpush4/MultiMain.java
@@ -4,17 +4,14 @@ import javax.swing.JFrame;
 
 public class MultiMain extends JFrame {
 
-  public static final int SCREEN_W_IN_TILES = 15;
-  public static final int SCREEN_H_IN_TILES = 15;
-
   public MultiMain() {
     add(new MultiPanel());
 
     setDefaultCloseOperation(EXIT_ON_CLOSE);
 
     setSize( //
-        MultiPanel.TILE_W * SCREEN_W_IN_TILES, //
-        MultiPanel.TILE_H * SCREEN_H_IN_TILES);
+        Constants.TILE_W * Constants.SCREEN_W_IN_TILES, //
+        Constants.TILE_H * Constants.SCREEN_H_IN_TILES);
 
     setVisible(true);
   }
