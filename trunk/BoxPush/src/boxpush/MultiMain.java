5b748ad1b5bc088ebd929c360be08ec77314e372
piojosnos@gmail.com <piojosnos@gmail.com@1b14608d-9101-1e4f-104e-7cb8ead80972>
2011-04-29 02:20:09 +0000

    Initial Commit Seems to work OK, needs more testing and code cleanup

---
+++ trunk/BoxPush/src/boxpush/MultiMain.java
@@ -0,0 +1,22 @@
+package boxpush;
+
+import javax.swing.JFrame;
+
+public class MultiMain extends JFrame {
+
+  public MultiMain() {
+    add(new MultiPanel());
+
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    setSize( //
+        Constants.TILE_W * Constants.SCREEN_W_IN_TILES, //
+        Constants.TILE_H * Constants.SCREEN_H_IN_TILES);
+
+    setVisible(true);
+  }
+
+  public static void main(String[] args) {
+    new MultiMain();
+  }
+}
