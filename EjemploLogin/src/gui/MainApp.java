6b4a537bdfc071d7ee8bbb8f9eb297a96bbd8a5f
piojosnos <piojosnos@gmail.com>
2011-06-14 04:36:55 +0000

    Major improvements (big refactor in all the project)

--- EjemploLogin/src/gui/MainApp.java
+++ EjemploLogin/src/gui/MainApp.java
@@ -7,15 +7,14 @@ import nextapp.echo.app.Window;
 /**
  * @author Anna Lezama
  */
-
 public class MainApp extends ApplicationInstance {
 
   public Window init() {
     Window window = new Window();
-    window.setTitle("Ejemplo de Conexiones");
+    window.setTitle("Ejemplo Echo3 + Hibernate");
 
     ContentPane contentPane = new ContentPane();
-    contentPane.add(new Welcome());
+    contentPane.add(new DesktopWelcome());
 
     window.setContent(contentPane);
 
