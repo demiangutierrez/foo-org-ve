9a925cd5139de3f1d59725dc6e2074ca8727739d
piojosnos <piojosnos@gmail.com>
2011-09-21 02:07:05 +0000

    Showing the use of WebMethod(exclude=true)

--- UnsortedSandBox/src_webservice/org/demo/ws/server/HelloWorld.java
+++ UnsortedSandBox/src_webservice/org/demo/ws/server/HelloWorld.java
@@ -1,11 +1,19 @@
 package org.demo.ws.server;
 
+import javax.jws.WebMethod;
 import javax.jws.WebService;
 
 @WebService
 public class HelloWorld {
 
+  @WebMethod
   public String sayHelloWorld(String name) {
     return "Hello World, " + name;
   }
+
+  @WebMethod(exclude = true)
+  public String ignoreThis(String name) {
+    return "Hello World, " + name;
+  }
+
 }
