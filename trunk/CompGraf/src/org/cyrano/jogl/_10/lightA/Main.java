c8d0cb8b219a20846d3e5cc22a423ca3bdbef9c0
piojosnos@gmail.com <piojosnos@gmail.com@1b14608d-9101-1e4f-104e-7cb8ead80972>
2011-11-08 13:02:09 +0000

    Initial Commit

---
+++ trunk/CompGraf/src/org/cyrano/jogl/_10/lightA/Main.java
@@ -0,0 +1,366 @@
+package org.cyrano.jogl._10.lightA;
+
+import java.awt.Dimension;
+import java.awt.Frame;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.media.opengl.GL;
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLCanvas;
+import javax.media.opengl.GLEventListener;
+import javax.media.opengl.glu.GLU;
+
+import org.cyrano.jogl.util.Matrix;
+import org.cyrano.jogl.util.MatrixOps;
+
+/**
+ * @author Demián Gutierrez
+ */
+public class Main implements GLEventListener, MouseListener, MouseMotionListener, KeyListener {
+
+  // --------------------------------------------------------------------------------
+  // Camera
+  // --------------------------------------------------------------------------------
+
+  private double topX = 0.0f;
+  private double topY = 1.0f;
+  private double topZ = 0.0f;
+
+  private double frnX = 0.0f;
+  private double frnY = 0.0f;
+  private double frnZ = 1.0f;
+
+  private double dist = 5;
+
+  // --------------------------------------------------------------------------------
+  // Camera movement
+  // --------------------------------------------------------------------------------
+
+  private float view_rotx = 0.0f;
+  //private float view_roty = 0.0f; // CHECK
+
+  private int prevMouseX;
+  private int prevMouseY;
+
+  // --------------------------------------------------------------------------------
+
+  private GLCanvas glCanvas;
+
+  // --------------------------------------------------------------------------------
+
+  public void init(GLAutoDrawable drawable) {
+    drawable.addMouseListener(this);
+    drawable.addMouseMotionListener(this);
+    drawable.addKeyListener(this);
+
+    GL gl = drawable.getGL();
+
+    gl.glDisable(GL.GL_CULL_FACE);
+    //gl.glEnable(GL.GL_CULL_FACE);
+    gl.glEnable(GL.GL_DEPTH_TEST);
+    gl.glEnable(GL.GL_LIGHTING);
+    
+    gl.glShadeModel(GL.GL_SMOOTH);
+  }
+
+  // --------------------------------------------------------------------------------
+
+  public void reshape(GLAutoDrawable drawable, //
+      int x, int y, int w, int h) {
+
+    GL gl = drawable.getGL();
+
+    gl.glViewport(0, 0, w, h);
+
+    calculatePerspective(gl);
+  }
+
+  // --------------------------------------------------------------------------------
+
+  private void calculatePerspective(GL gl) {
+    GLU glu = new GLU();
+
+    gl.glMatrixMode(GL.GL_PROJECTION);
+
+    gl.glLoadIdentity();
+
+    double nr = 0.5;
+    double fr = 2 * (dist - nr) + nr;
+
+    //System.err.println("nr: " + nr);
+    //System.err.println("fr: " + fr);
+    //System.err.println("dist: " + dist);
+
+    glu.gluPerspective(90, 1, nr, fr);
+
+    gl.glMatrixMode(GL.GL_MODELVIEW);
+  }
+
+  // --------------------------------------------------------------------------------
+
+  public void display(GLAutoDrawable drawable) {
+    GL gl = drawable.getGL();
+    GLU glu = new GLU();
+
+    calculatePerspective(gl);
+
+    gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+
+    gl.glLoadIdentity();
+
+    double frnDstX = frnX * dist;
+    double frnDstY = frnY * dist;
+    double frnDstZ = frnZ * dist;
+
+    glu.gluLookAt(frnDstX, frnDstY, frnDstZ, 0, 0, 0, topX, topY, topZ);
+
+    //    +--+
+    //    |ma|
+    // +--+--+--+
+    // |ce|ve|ro|
+    // +--+--+--+
+    //    |am|
+    //    +--+
+    //    |az|
+    //    +--+
+    //glu.gluLookAt(+2, +2, +2, 0, 0, 0, 0, 1, 0); // VE / MA / RO
+    //glu.gluLookAt(-2, +2, +2, 0, 0, 0, 0, 1, 0); // CE / MA / VE
+    //glu.gluLookAt(-2, -2, +2, 0, 0, 0, 0, 1, 0); // CE / AM / VE
+    //glu.gluLookAt(+2, -2, +2, 0, 0, 0, 0, 1, 0); // VE / AM / RO
+
+    //    +--+
+    //    |ma|
+    // +--+--+--+
+    // |ro|az|ce|
+    // +--+--+--+
+    //    |am|
+    //    +--+
+    //    |ve|
+    //    +--+
+    //glu.gluLookAt(+2, +2, -2, 0, 0, 0, 0, 1, 0); // RO / MA / AZ
+    //glu.gluLookAt(-2, +2, -2, 0, 0, 0, 0, 1, 0); // AZ / MA / CE
+    //glu.gluLookAt(-2, -2, -2, 0, 0, 0, 0, 1, 0); // AZ / AM / CE
+    //glu.gluLookAt(+2, -2, -2, 0, 0, 0, 0, 1, 0); // RO / AM / CE
+
+    gl.glPushMatrix();
+    gl.glColor3f(0, 0, 1); // AZ
+    gl.glRotatef(180f, 0, 1, 0);
+    drawFace(gl);
+    gl.glPopMatrix();
+
+    gl.glPushMatrix();
+    gl.glColor3f(0, 1, 0); // VE
+    gl.glRotatef(0f, 0, 1, 0);
+    drawFace(gl);
+    gl.glPopMatrix();
+
+    gl.glPushMatrix();
+    gl.glColor3f(0, 1, 1); // CE
+    gl.glRotatef(-90f, 0, 1, 0);
+    drawFace(gl);
+    gl.glPopMatrix();
+
+    gl.glPushMatrix();
+    gl.glColor3f(1, 0, 0); // RO
+    gl.glRotatef(90f, 0, 1, 0);
+    drawFace(gl);
+    gl.glPopMatrix();
+
+    gl.glPushMatrix();
+    gl.glColor3f(1, 0, 1); // MA
+    gl.glRotatef(-90f, 1, 0, 0);
+    drawFace(gl);
+    gl.glPopMatrix();
+
+    gl.glPushMatrix();
+    gl.glColor3f(1, 1, 0); // AM
+    gl.glRotatef(90f, 1, 0, 0);
+    drawFace(gl);
+    gl.glPopMatrix();
+
+    gl.glFlush();
+  }
+
+  // --------------------------------------------------------------------------------
+
+  private void drawFace(GL gl) {
+    gl.glBegin(GL.GL_QUADS);
+    gl.glVertex3f(-0.5f, -0.5f, +0.5f);
+    gl.glVertex3f(+0.5f, -0.5f, +0.5f);
+    gl.glVertex3f(+0.5f, +0.5f, +0.5f);
+    gl.glVertex3f(-0.5f, +0.5f, +0.5f);
+    gl.glEnd();
+  }
+
+  // --------------------------------------------------------------------------------
+
+  public void displayChanged(GLAutoDrawable drawable, //
+      boolean modeChanged, boolean deviceChanged) {
+    // Empty
+  }
+
+  // --------------------------------------------------------------------------------
+  // MouseListener
+  // --------------------------------------------------------------------------------
+
+  public void mouseEntered(MouseEvent e) {
+    // Empty
+  }
+
+  public void mouseExited(MouseEvent e) {
+    // Empty
+  }
+
+  public void mousePressed(MouseEvent e) {
+    prevMouseX = e.getX();
+    prevMouseY = e.getY();
+  }
+
+  public void mouseReleased(MouseEvent e) {
+    // Empty
+  }
+
+  public void mouseClicked(MouseEvent e) {
+    // Empty
+  }
+
+  // --------------------------------------------------------------------------------
+  // MouseMotionListener
+  // --------------------------------------------------------------------------------
+
+  public void mouseDragged(MouseEvent e) {
+    int x = e.getX();
+    int y = e.getY();
+    Dimension size = e.getComponent().getSize();
+
+    float thetaY = 360.0f * ((float) (prevMouseX - x) / (float) size.width);
+    float thetaX = 360.0f * ((float) (prevMouseY - y) / (float) size.height);
+
+    prevMouseX = x;
+    prevMouseY = y;
+
+    // thetaX = 0; // FOR DEBUG
+
+    if ((view_rotx - thetaX) > +60) {
+      thetaX = view_rotx - 60;
+    }
+
+    if ((view_rotx - thetaX) < -60) {
+      thetaX = view_rotx + 60;
+    }
+
+    view_rotx -= thetaX;
+    //view_roty += thetaY;
+
+    //System.err.println(thetaX + ";" + thetaY);
+
+    rotate(thetaX, thetaY);
+
+    glCanvas.repaint();
+  }
+
+  // --------------------------------------------------------------------------------
+
+  public void rotate(float thetaX, float thetaY) {
+    Matrix curFrn = new Matrix(4, 1);
+    curFrn.setData( //
+        new double[]{frnX, frnY, frnZ, 1});
+
+    Matrix curTop = new Matrix(4, 1);
+    curTop.setData( //
+        new double[]{topX, topY, topZ, 1});
+
+    Matrix ry = MatrixOps.rotateY(thetaY);
+
+    Matrix newFrn = MatrixOps.matrixMult(ry, curFrn);
+    Matrix newTop = MatrixOps.matrixMult(ry, curTop);
+
+    curFrn = newFrn;
+    curTop = newTop;
+
+    Matrix curSid = MatrixOps.crossProduct(curFrn, curTop);
+
+    Matrix rs = MatrixOps.rotate( //
+        -thetaX, curSid.get(0, 0), curSid.get(1, 0), curSid.get(2, 0));
+
+    newFrn = MatrixOps.matrixMult(rs, curFrn);
+    newTop = MatrixOps.matrixMult(rs, curTop);
+
+    frnX = newFrn.get(0, 0);
+    frnY = newFrn.get(1, 0);
+    frnZ = newFrn.get(2, 0);
+
+    topX = newTop.get(0, 0);
+    topY = newTop.get(1, 0);
+    topZ = newTop.get(2, 0);
+  }
+
+  // --------------------------------------------------------------------------------
+
+  public void mouseMoved(MouseEvent e) {
+    // Empty
+  }
+
+  // --------------------------------------------------------------------------------
+  // KeyListener
+  // --------------------------------------------------------------------------------
+
+  public void keyTyped(KeyEvent evt) {
+    switch (evt.getKeyChar()) {
+      case 'A' :
+      case 'a' :
+        if (dist <= 10) {
+          dist *= 2;
+        }
+        break;
+      case 'Z' :
+      case 'z' :
+        if (dist >= 5) {
+          dist /= 2;
+        }
+        break;
+    }
+
+    glCanvas.repaint();
+  }
+
+  // --------------------------------------------------------------------------------
+
+  public void keyPressed(KeyEvent e) {
+    //  Empty    
+  }
+
+  // --------------------------------------------------------------------------------
+
+  public void keyReleased(KeyEvent e) {
+    // Empty    
+  }
+
+  // --------------------------------------------------------------------------------
+
+  public static void main(String[] args) {
+    Frame frame = new Frame("JOGL Main");
+
+    GLCanvas canvas = new GLCanvas();
+    Main m = new Main();
+    m.glCanvas = canvas;
+    canvas.addGLEventListener(m);
+
+    frame.add(canvas);
+    frame.setSize(300, 300);
+    frame.setVisible(true);
+
+    frame.addWindowListener(new WindowAdapter() {
+      public void windowClosing(WindowEvent e) {
+        System.exit(0);
+      }
+    });
+  }
+}
